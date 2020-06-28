92502f58eeb4de7ca95ef9d9d7efaa3a37d62fce
Lief Friedrichs <lief.friedrichs@gmail.com>
2020-06-28 12:41:00 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -11,7 +11,7 @@ text.length > 4 ? true : false
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-text.match(/^[A-Z][a-z]*[!.]/) ? true : false
+text.match(/^[A-Z][a-z]*[!.?]/) ? true : false
 end
 
 def valid_phone_number?(phone)
