bd155f31f6e824dd2a52aaec08e10c41544bb351
Lief Friedrichs <lief.friedrichs@gmail.com>
2020-06-28 12:49:47 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -4,7 +4,7 @@ end
 
 def words_starting_with_un_and_ending_with_ing(text)
   array = text.split(' ')
-  array.select {|word| word.length > 4}
+  array.select {|word| word.size > 4}
 end
 
 def words_five_letters_long(text)
