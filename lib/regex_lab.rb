6c3921185d17ee8957da9ba48a03fd6ddcee0ba0
Lief Friedrichs <lief.friedrichs@gmail.com>
2020-06-28 12:44:09 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -3,7 +3,7 @@ def starts_with_a_vowel?(word)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-  text.match(/[^un].[ing$]/) ? true : false
+  text.match(/^[un].[ing]$/) ? true : false
 end
 
 def words_five_letters_long(text)
@@ -11,7 +11,7 @@ text.length > 4 ? true : false
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-text.match(/^[A-Z].*[!.?]/) ? true : false
+text.match(/^[A-Z].*[!.?]$/) ? true : false
 end
 
 def valid_phone_number?(phone)
