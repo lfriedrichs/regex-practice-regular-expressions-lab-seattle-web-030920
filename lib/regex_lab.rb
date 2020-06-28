84eae5b55b5de70a4968b25333525c2ac0007c15
Lief Friedrichs <lief.friedrichs@gmail.com>
2020-06-28 12:46:59 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -3,7 +3,8 @@ def starts_with_a_vowel?(word)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-  text.split(' ')
+  array = text.split(' ')
+  array.filter(|word| word.match)
 end
 
 def words_five_letters_long(text)
