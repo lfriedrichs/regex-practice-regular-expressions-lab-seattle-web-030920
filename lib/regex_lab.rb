444e947e11944b15d1c8d34fbc0bb69b6fc7db93
Lief Friedrichs <lief.friedrichs@gmail.com>
2020-06-28 12:28:12 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -7,13 +7,13 @@ def words_starting_with_un_and_ending_with_ing(text)
 end
 
 def words_five_letters_long(text)
-
+word.match(/aeiou/) ? true : false
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+word.match(/aeiou/) ? true : false
 end
 
 def valid_phone_number?(phone)
-
+word.match(/aeiou/) ? true : false
 end
