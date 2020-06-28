c5075102a4d2e6e6fbb758f1ba6b3073e8ff1970
Lief Friedrichs <lief.friedrichs@gmail.com>
2020-06-28 12:51:49 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -8,7 +8,8 @@ def words_starting_with_un_and_ending_with_ing(text)
 end
 
 def words_five_letters_long(text)
-count(text) > 4 ? true : false
+  array = text.split(' ')
+  array.select {|word| word.size > 4}
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
